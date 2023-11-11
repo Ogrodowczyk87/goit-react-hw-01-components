543c0794ddaf4e36e1dd955004d14ac8f0fa169e
Rafal Ogrodowczyk <r.ogrodowczyk87@gmail.com>
2023-11-11 21:53:40 +0000

    title

--- src/components/statistics/statistics.jsx
+++ src/components/statistics/statistics.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import PropTypes from 'prop-types';
 
 
-export default function statistics({ stats }) {
+export default function statistics({ stats, title }) {
     return <div>
         <section class="statistics">
-            <h2 class="title">Upload stats</h2>
+            <h2 class="title">{title}</h2>
             <statList>
                 {stats.map(el => (
                     <item key={el.id}>
