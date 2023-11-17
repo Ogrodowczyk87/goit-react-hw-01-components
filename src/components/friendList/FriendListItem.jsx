31a17c1a8b48363a57e2ceafa06a89c7a706a972
Rafal Ogrodowczyk <r.ogrodowczyk87@gmail.com>
2023-11-17 06:10:37 +0000

    r

---
+++ src/components/friendList/FriendListItem.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import css from "./FriendList.module.css"
+import PropTypes from 'prop-types';
+
+export default function FriendListItem({ isOnline, avatar, name }) {
+    return (
+
+        <li className={css.item}>
+            <span className={isOnline ? css.status__true : css.status__false}></span>
+            <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
+            <p className={css.name}> {name}</p>
+        </li>
+    )
+}
+
+FriendListItem.prototype = {
+    isOnline: PropTypes.bool.isRequired,
+    avatar: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+};
+
---
+++ src/components/statistics/Statistics.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import css from "./Statistic.module.css"
+
+export default function Statistics({ stats, title }) {
+
+
+    return <div>
+        <section className={css.statistics}>
+            {title && <h2 className={css.title}>{title}</h2>}
+            {/* <h2 className={css.title}>{title}</h2> */}
+            <ul className={css.statList}>
+                {stats.map(el => (
+                    <item className={css.item} key={el.id}>
+                        <label className={css.label}>{el.label}</label>
+                        <percentage className={css.percentage}>{el.percentage}</percentage>
+                    </item>
+                ))}
+            </ul>
+
+        </section>
+    </div>;
+}
+
+Statistics.propTypes = {
+    title: PropTypes.string,
+    isOnline: PropTypes.bool.isRequired,
+    stats: PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        label: PropTypes.string.isRequired,
+        percentage: PropTypes.number.isRequired
+    })
+};
+
+
