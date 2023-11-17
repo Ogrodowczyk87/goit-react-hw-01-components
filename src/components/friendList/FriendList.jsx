f85356d374d8af42b5fb8fa68fae19b3c0c28eee
Rafal Ogrodowczyk <r.ogrodowczyk87@gmail.com>
2023-11-17 05:52:19 +0000

    add files

---
+++ src/components/friendList/FriendList.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import FriendListItem from "./FriendListItem";
+import PropTypes from "prop-types";
+
+
+export default function FriendList({ friends }) {
+    return <list>
+        {friends.map(({ avatar, name, isOnline, id }) => (
+            <FriendListItem
+                key={id}
+                avatar={avatar}
+                name={name}
+                isOnline={isOnline}
+            />
+        ))}
+    </list>
+}
+
+
+FriendListItem.propTypes = {
+    avatar: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    isOnline: PropTypes.bool.isRequired,
+};
+
